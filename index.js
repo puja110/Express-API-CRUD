7202c8db4d3e988f8a03221c779aa29ab51fec9a
puja110 <csta.puja@gmail.com>
2024-02-14 16:49:15 -0500

    comment header added with filename, student name, studentId, and Date

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * Filename: index.js
+ * Student Name / Student ID:
+ * Puja Shrestha, 200573293
+ * Suyog Shrestha, 200565523
+ * Date: Feb 14th, 2024
+ */
+
 // importing dependencies
 const express = require('express');
 const mongoose = require('mongoose');
@@ -17,11 +25,11 @@ InitiateMongoServer();
 
 // root route setup
 app.get('/', function(req, res) {
-    res.send("Welcome to the assignment 2 - express api crud");
+    res.send("Welcome to the Assignment 2 - Express Api - CRUD operations");
 });
 
 // create one port no on which app listen
 const port = process.env.PORT || 4000;
 app.listen(port, function() {
-    console.log("App is listening to port no 3000");
+    console.log("App is listening to port no 4000");
 });
