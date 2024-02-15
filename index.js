f3f67c59c0c6167f392955e431fac74308bf81c3
Suyog Shrestha <suyogshrestha.tech@gmail.com>
2024-02-15 18:40:22 -0500

    Comments and Documentation Updated

--- index.js
+++ index.js
@@ -8,7 +8,6 @@
 
 // importing dependencies
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const fs = require('fs');
@@ -23,13 +22,16 @@ const InitiateMongoServer = require('./db');
 // initialize Mongodb connection
 InitiateMongoServer();
 
-// root route setup
-app.get('/', function(req, res) {
-    res.send("Welcome to the Assignment 2 - Express Api - CRUD operations");
+/**
+ * @param {*} req
+ * @param {*} res
+ * @description Root route setup
+ */
+app.get('/', function (req, res) {
+  res.send('Welcome to the Assignment 2 - Express Api - CRUD operations');
 });
 
-// create one port no on which app listen
 const port = process.env.PORT || 4000;
-app.listen(port, function() {
-    console.log("App is listening to port no 4000");
+app.listen(port, function () {
+  console.log('App is listening to port no 4000');
 });
