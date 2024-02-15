c8c2d361a3433940f5a751b253e393b9d699a074
Suyog Shrestha <suyogshrestha.tech@gmail.com>
2024-02-15 18:40:34 -0500

    Comments and Documentation Updated

--- src/model/books.js
+++ src/model/books.js
@@ -1,21 +1,17 @@
-// importing required modules
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // defining the BooksSchema
 const BooksSchema = new Schema({
-    id: {type: String}, // defines the bookId field as a String type
-    bookName: {type: String}, // defines the bookName field as a String type
-    isbn: {type: String}, // defines the isbn field as a String type
-    rating: {type: Number}, // defines the rating field as a Number type
-    author: {type: String}, // defines the author field as a String type
-    genre: [ // defines the genre field as an array of Strings
-        {type: String}
-    ],
-})
+  id: { type: String },
+  bookName: { type: String },
+  isbn: { type: String },
+  rating: { type: Number },
+  author: { type: String },
+  genre: [{ type: String }],
+});
 
 // creating a Mongoose model named 'Books'
 const Book = mongoose.model('Books', BooksSchema);
 
-// exporting the Book model to be used in other files
 module.exports = Book;
