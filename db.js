1b7a691e2319c58717fc9e3a9d19cd9c4d7cd6a8
Suyog Shrestha <suyogshrestha.tech@gmail.com>
2024-02-15 18:39:45 -0500

    [Added] 10 Book data inserted after DB connection

--- db.js
+++ db.js
@@ -1,31 +1,37 @@
-// importing required modules
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const Book = require('./src/model/books');
+const { books } = require('./src/data/favBookData');
 
 // loading environment variables from config file
 dotenv.config({ path: './config.env' });
 
-// getting MongoDB connection URI from environment variabl
 const mongoURI = process.env.MONGODB_URL;
 
 // creating Express application
 const app = express();
 app.use(express.json());
 
-// function to initiate connection to MongoDB server
-const InitiateMongoServer = async() => {
-    try {
-        // connecting to MongoDB using the connection URI
-        await mongoose.connect(mongoURI, {
+/**
+ * @description function to initiate connection to MongoDB server
+ */
+const InitiateMongoServer = async () => {
+  try {
+    // connecting to MongoDB using the connection URI
+    await mongoose.connect(mongoURI, {});
+    console.log('Connected to DB!');
 
-        });
-        console.log("Connected to DB!");
-    } catch(e) {
-        console.log(e);
-        throw e;
+    const count = await Book.countDocuments();
+    if (count === 0) {
+      // Insert initial Books data
+      await Book.insertMany(books);
+      console.log('Initial data inserted successfully');
     }
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
 };
 
-// exporting the function to initiate MongoDB server connection
 module.exports = InitiateMongoServer;
