eab7e0a23d69db33555d022f97d06be1e2c5893a
Suyog Shrestha <suyogshrestha.tech@gmail.com>
2024-02-15 20:22:31 -0500

    Model Updated

--- src/model/books.js
+++ src/model/books.js
@@ -4,11 +4,11 @@ const Schema = mongoose.Schema;
 // defining the BooksSchema
 const BooksSchema = new Schema({
   id: { type: String },
-  bookName: { type: String },
+  name: { type: String },
   isbn: { type: String },
   rating: { type: Number },
   author: { type: String },
-  genre: [{ type: String }],
+  genre: { type: String },
 });
 
 // creating a Mongoose model named 'Books'
